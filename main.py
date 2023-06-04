9a21cace6d143795f382c864378cb6594088b08f
xqy2006 <48169260+xqy2006@users.noreply.github.com>
2023-06-04 15:40:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ print("version:"+version)
 wget.download(s['downloadCN'],"./"+name)
 os.system("innounp -x -m "+name)
 os.system("pip install frida frida-tools")
-os.system("frida "./{app}/Typora.exe" -l "./unpack.js" -q -t 10")
+os.system("frida \"./{app}/Typora.exe\" -l \"./unpack.js\" -q -t 10")
 os.system("npm install asar -g")
 os.system("asar extract ./{app}/resources/app.asar ./{app}/resources/app")
 
